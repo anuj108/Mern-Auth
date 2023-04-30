ecbf612a98e12bf7b4cd0809756e0de89d6c141b
AnUj <shakya.anuj910@gmail.com>
2023-04-30 20:48:39 +0530

    state handled of signup

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
 import { Box, Button, TextField, Typography } from '@mui/material';
 
 const Signup = () => {
-    const handleSubmit=()=>{};
+  const [inputs, setInputs] = useState({
+    name:"",
+    email:"",
+    password:"",
+  })
+    const handleSubmit=(e)=>{
+      e.preventDefault();
+      console.log(inputs);
+    };
+    const handleChange=(e)=>{
+      setInputs((prev)=>({
+        ...prev,
+        [e.target.name]:e.target.value
+      }))
+      
+    }
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <Box marginLeft="auto" marginRight="auto" width={300} justifyContent="center" alignItems="center" display="flex" flexDirection={"column"}>
         <Typography variant="h2">Signup</Typography>
 
-            <TextField variant="outlined" placeholder="name" margin="normal"/>
-            <TextField variant="outlined" placeholder="email" margin="normal"/>
-            <TextField variant="outlined" placeholder="password" margin="normal"/>
+            <TextField name="name" onChange={handleChange} value={inputs.name} type="text" variant="outlined" placeholder="name" margin="normal"/>
+            <TextField name="email" onChange={handleChange} value={inputs.email} type="email" variant="outlined" placeholder="email" margin="normal"/>
+            <TextField name="password" onChange={handleChange} value={inputs.password} type="password" variant="outlined" placeholder="password" margin="normal"/>
             <Button variant="contained" color="success" type="submit">Signup</Button>
         </Box>
       </form>
